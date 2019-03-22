cf39457660f70ae151cd752724c1d17ba37ff648
Shanki <shanki.ranju@gmail.com>
2019-03-22 12:51:11 -0500

    m

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 // Initialize Express
 var app = express();
